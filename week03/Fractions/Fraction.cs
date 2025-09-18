5e460f152d5537ef92931297a73e38ac681a1e56
origamijellybean <thunter@byupathway.edu>
2025-09-17 21:44:11 -0600

    prepare program

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,50 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return Convert.ToDouble(_top) / Convert.ToDouble(_bottom);
+    }
+    
+
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction number = new Fraction();
+        Fraction number2 = new Fraction(5);
+        Fraction number3 = new Fraction(3, 4);
+        Fraction number4 = new Fraction(1, 3);
+        Console.WriteLine(number.GetFractionString());
+        Console.WriteLine(number.GetDecimalValue());
+        Console.WriteLine(number2.GetFractionString());
+        Console.WriteLine(number2.GetDecimalValue());
+        Console.WriteLine(number3.GetFractionString());
+        Console.WriteLine(number3.GetDecimalValue());
+        Console.WriteLine(number4.GetFractionString());
+        Console.WriteLine(number4.GetDecimalValue());
     }
 }
