2f054ba1b27d8a3d05a010795faddba4c2514383
origamijellybean <thunter@byupathway.edu>
2025-10-04 00:54:16 -0600

    fixed the stye of variable

--- week05/Mindfulness/RandomSelection.cs
+++ week05/Mindfulness/RandomSelection.cs
@@ -5,7 +5,7 @@ public class RandomSelection
 {
     private List<string> _list = [];
     private List<string> _scrambleList = [];
-    private int position = -1;
+    private int _position = -1;
 
     public RandomSelection(List<string> list)
     {
@@ -20,10 +20,10 @@ public class RandomSelection
 
     public string GetNextItem()
     {
-        if (position < _scrambleList.Count() - 1)
-            position += 1;
+        if (_position < _scrambleList.Count() - 1)
+            _position += 1;
         else
-            position = 0;
-        return _scrambleList[position];
+            _position = 0;
+        return _scrambleList[_position];
     }
     }
